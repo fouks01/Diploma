6120b3f1ad3055e570c83afdbbdc96876619aa42
Alex Tikhonov <tau9501@gmail.com>
2022-05-15 19:22:49 +0300

    Added Auto-slider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import modal from './modules/modal';
 import accordeon from './modules/accordeon';
 import scroll from './modules/scroll';
+import slider from './modules/slider';
+
 
 
 
 modal();
 accordeon();
 scroll();
+slider();
---
+++ src/modules/slider.js
@@ -0,0 +1,72 @@
+const slider = () => {
+    const sliderBlock = document.querySelector('.top-slider');
+    const slides = sliderBlock.querySelectorAll('.item');
+    const timerInterval = 3000;
+
+
+    let currentSlide = 0;
+    let interval;
+
+    const prevSlide = (elems, index, strClass) => {
+        elems[index].classList.remove(strClass);
+        elems[index].style.display = "none";
+    };
+
+    const nextSlide = (elems, index, strClass) => {
+        elems[index].classList.add(strClass);
+        elems[index].style.display = "block";
+    };
+
+
+    const autoSlide = () => {
+        prevSlide(slides, currentSlide, 'item-active');
+
+        currentSlide++;
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        };
+
+        nextSlide(slides, currentSlide, 'item-active');
+    };
+
+    const startSlide = (timer = 1500) => {
+        interval = setInterval(autoSlide, timer);
+    };
+
+    const stopSlide = () => {
+        clearInterval(interval);
+    };
+
+    sliderBlock.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        prevSlide(slides, currentSlide, 'item-active');
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        };
+
+        if (currentSlide < 0) {
+            currentSlide = slides.length - 1;
+        }
+
+        nextSlide(slides, currentSlide, 'item-active');
+    });
+
+    sliderBlock.addEventListener('mouseenter', (e) => {
+        if (e.target.matches('.item')) {
+            stopSlide(timerInterval);
+        }
+    }, true);
+
+    sliderBlock.addEventListener('mouseleave', (e) => {
+        if (e.target.matches('.item')) {
+            startSlide(timerInterval);
+        }
+    }, true);
+
+    startSlide(timerInterval);
+};
+
+export default slider;
