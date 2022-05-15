dc0257dcd4520204fc796ae49bb1980947e53bdf
Alex Tikhonov <tau9501@gmail.com>
2022-05-16 01:21:05 +0300

    Added soft scrolling

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,5 +1,6 @@
 const scroll = () => {
     const linkUp = document.querySelector('.up');
+    const anchors = document.querySelectorAll('a[href*="#"]');
 
 
     linkUp.style.display = "none";
@@ -18,6 +19,20 @@ const scroll = () => {
             linkUp.style.display = "block";
         }
     });
+
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href').substr(1);
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
 };
 
 export default scroll;
